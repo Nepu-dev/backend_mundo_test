6781e65128e864ca5de61b3b63bc52a104e7e624
MatiasARSdev <105838341+MatiasARSdev@users.noreply.github.com>
2023-04-21 17:29:58 -0400

    21/04

--- app/Http/Controllers/MarcasController.php
+++ app/Http/Controllers/MarcasController.php
@@ -27,7 +27,7 @@ class MarcasController extends Controller
 
         return response()->json($modelos);
     }
-    public function listarDispositivos($bodega)
+    public function listarDispositivosPorBodega($bodega)
     {
         $dispositivos = DB::select('SELECT dispositivo.id, dispositivo.nombre, marca.nombre as marca, modelo.nombre as modelo, bodega.nombre as bodega
         from dispositivo join modelo on dispositivo.id_modelo = modelo.id join marca on modelo.id_marca = marca.id join bodega on dispositivo.id_bodega = bodega.id
@@ -35,11 +35,71 @@ class MarcasController extends Controller
 
         return response()->json($dispositivos);
     }
-    public function listarDispositivosFilter($modelo, $marca, $bodega)
+    public function listarDispositivosPorMarca($marca)
     {
         $dispositivos = DB::select('SELECT dispositivo.id, dispositivo.nombre, marca.nombre as marca, modelo.nombre as modelo, bodega.nombre as bodega
         from dispositivo join modelo on dispositivo.id_modelo = modelo.id join marca on modelo.id_marca = marca.id join bodega on dispositivo.id_bodega = bodega.id
-        where modelo.id = ? or marca.id = ? and bodega.id = ?', [$modelo, $marca, $bodega]);
+        where marca.id = ?', [$marca]);
+
+        return response()->json($dispositivos);
+    }
+    public function listarDispositivosPorModelo($modelo)
+    {
+        $dispositivos = DB::select('SELECT dispositivo.id, dispositivo.nombre, marca.nombre as marca, modelo.nombre as modelo, bodega.nombre as bodega
+        from dispositivo join modelo on dispositivo.id_modelo = modelo.id join marca on modelo.id_marca = marca.id join bodega on dispositivo.id_bodega = bodega.id
+        where modelo.id = ?', [$modelo]);
+
+        return response()->json($dispositivos);
+    }
+    //------------------------------------------------------------------------------------------
+    public function listarDispositivosPorMarcayModelo($marca, $modelo)
+    {
+        $dispositivos = DB::select('SELECT dispositivo.id, dispositivo.nombre, marca.nombre as marca, modelo.nombre as modelo, bodega.nombre as bodega
+        from dispositivo join modelo on dispositivo.id_modelo = modelo.id join marca on modelo.id_marca = marca.id join bodega on dispositivo.id_bodega = bodega.id
+        where marca.id = ? and modelo.id = ?', [$marca, $modelo]);
+
+        return response()->json($dispositivos);
+    }
+    public function listarDispositivosPorBodegayModelo($bodega, $modelo)
+    {
+        $dispositivos = DB::select('SELECT dispositivo.id, dispositivo.nombre, marca.nombre as marca, modelo.nombre as modelo, bodega.nombre as bodega
+        from dispositivo join modelo on dispositivo.id_modelo = modelo.id join marca on modelo.id_marca = marca.id join bodega on dispositivo.id_bodega = bodega.id
+        where bodega.id = ? and modelo.id = ?', [$bodega, $modelo]);
+
+        return response()->json($dispositivos);
+    }
+    public function listarDispositivosPorBodegayMarca($marca, $bodega)
+    {
+        $dispositivos = DB::select('SELECT dispositivo.id, dispositivo.nombre, marca.nombre as marca, modelo.nombre as modelo, bodega.nombre as bodega
+        from dispositivo join modelo on dispositivo.id_modelo = modelo.id join marca on modelo.id_marca = marca.id join bodega on dispositivo.id_bodega = bodega.id
+        where marca.id = ? and bodega.id = ?', [$marca, $bodega]);
+
+        return response()->json($dispositivos);
+    }
+    public function listarDispositivosFilter($modelo, $marca, $bodega)
+    {
+        
+        if ($marca == 0 && $modelo == 0) {
+            return $this->listarDispositivosPorBodega($bodega);
+        };
+        if ($bodega == 0 && $modelo == 0) {
+            return $this->listarDispositivosPorMarca($marca);
+        };
+        if ($marca == 0 && $bodega == 0) {
+            return $this->listarDispositivosPorModelo($modelo);
+        };
+        if ($marca != 0 && $modelo != 0 && $bodega == 0) {
+            return $this->listarDispositivosPorMarcayModelo($marca, $modelo);
+        };
+        if ($bodega != 0 && $modelo != 0 && $marca == 0) {
+            return $this->listarDispositivosPorBodegayModelo($bodega, $modelo);
+        };
+        if ($marca != 0 && $bodega != 0 && $modelo == 0) {
+            return $this->listarDispositivosPorBodegayMarca($marca, $bodega);
+        };
+        $dispositivos = DB::select('SELECT dispositivo.id, dispositivo.nombre, marca.nombre as marca, modelo.nombre as modelo, bodega.nombre as bodega
+        from dispositivo join modelo on dispositivo.id_modelo = modelo.id join marca on modelo.id_marca = marca.id join bodega on dispositivo.id_bodega = bodega.id
+        where modelo.id = ? and marca.id = ? and bodega.id = ?', [$modelo, $marca, $bodega]);
 
         return response()->json($dispositivos);
     }
